bb236eb386c926ee942079735a5772f920d7e923
Niko Ziozis <nziozis@cs.stonybrook.edu>
2019-11-26 12:41:23 -0500

    Revert "p1"
    This reverts commit 3fd5f0bfe25e43c68ea67d25f6959a970e33d322.

--- majmin/src/main/java/mm_districting/Joinability.java
+++ majmin/src/main/java/mm_districting/Joinability.java
@@ -2,8 +2,4 @@ package mm_districting;
 
 public class Joinability {
 
-
-    public double claculateMajMinJoinabillity(Cluster cluster, minThresh, MaxThresh, Race minority){
-	return 0.5;
-    }
 }
